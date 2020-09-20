02ca6f40191449bd25c5d3eb7bb11edc29e5b0f9
Yasir-Tbaileh <yasur96@gmail.com>
2020-09-20 13:20:25 +0300

    styling the grid container

--- data-base.js
+++ data-base.js
@@ -16,17 +16,28 @@ function each(coll, fn) {
 
 // create the game interface
 function create(arr, fn) {
-	each(arr, function(elem, i) {
-		var frame = $('#frame')
-		var elem = $('<div class="imgs"></div>')
-		frame.append(elem)
+	each(arr, function (elem, i) {
+		var frame = $('#frame');
+		var elem1 = $('<div class="imgs"></div>');
+		
+		frame.append(elem1);
 	})
 }
+// function create(arr) {
+// 	each(arr, function(elem, i) {
+// 		var frame = $('#frame');
+// 		var elem1 = $('<div class="imgs"></div>');
+// 		// if(elem > 8){
+// 		// 	elem -= 8;
+// 		// }
+// 		elem1.attr("class", elem);
+// 		//elem1.html('<img scr="images[elem]">');
+// 		frame.append(elem1);
+		
+// 	});
+// }
 
+//
 
-
-
-
-
-// 
-var images = ["https://www.flaticon.com/svg/static/icons/svg/3456/3456656.svg", "https://www.flaticon.com/svg/static/icons/svg/3456/3456709.svg","https://www.flaticon.com/svg/static/icons/svg/3456/3456669.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456847.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456890.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456805.svg","https://www.flaticon.com/premium-icon/icons/svg/3457/3457010.svg", "https://www.flaticon.com/premium-icon/icons/svg/3456/3456953.svg" ]
+/*
+var images = ["https://www.flaticon.com/svg/static/icons/svg/3456/3456656.svg", "https://www.flaticon.com/svg/static/icons/svg/3456/3456709.svg","https://www.flaticon.com/svg/static/icons/svg/3456/3456669.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456847.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456890.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456805.svg","https://www.flaticon.com/premium-icon/icons/svg/3457/3457010.svg", "https://www.flaticon.com/premium-icon/icons/svg/3456/3456953.svg" ]*/
