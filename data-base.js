f69ec07f717e9970f4fe9c280637f880dac99ceb
RamiOkkeh <ramiokkeh@gmail.com>
2020-09-21 09:54:56 +0300

    documneting the code, keeping track of user score, fixed bug in randomizing imgs

--- data-base.js
+++ data-base.js
@@ -1,6 +1,12 @@
+
+// image sources
+var images = ["https://www.flaticon.com/svg/static/icons/svg/3456/3456656.svg", "https://www.flaticon.com/svg/static/icons/svg/3456/3456709.svg","https://www.flaticon.com/svg/static/icons/svg/3456/3456669.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456847.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456890.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456805.svg","https://www.flaticon.com/premium-icon/icons/svg/3457/3457010.svg", "https://www.flaticon.com/premium-icon/icons/svg/3456/3456953.svg", "https://www.flaticon.com/svg/static/icons/svg/3456/3456656.svg", "https://www.flaticon.com/svg/static/icons/svg/3456/3456709.svg","https://www.flaticon.com/svg/static/icons/svg/3456/3456669.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456847.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456890.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456805.svg","https://www.flaticon.com/premium-icon/icons/svg/3457/3457010.svg", "https://www.flaticon.com/premium-icon/icons/svg/3456/3456953.svg" ]
+
+
 // helper functions 
-// =======================
-// writing each function
+// ===================================
+
+// each function for itiration
 function each(coll, fn) {
 	if (Array.isArray(coll) || typeof coll === 'string'){
 		for (var i = 0; i < coll.length; i++) {
@@ -12,7 +18,7 @@ function each(coll, fn) {
 		}
 	}
 }
-
+// reduce function for ... i guess nothing...
 function reduce(arr, fn, start) {
   var acc = start
   each(arr, function(elem, i) {
@@ -20,7 +26,7 @@ function reduce(arr, fn, start) {
   })
   return acc
 }
-
+// range function for img id's
 function range(min, max) {
 	var arr = []
 	if (max === undefined) {
@@ -32,81 +38,12 @@ function range(min, max) {
 	}
 	return arr
 }
-
+// random number gen. for randomizing imgs
 function randomize(min, max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function create(arr, fn) {
-	var imagesCopy = images.slice(0);
-	$('#frame').empty();
-	each(arr, function (elem, i) {
-		
-		var frame = $('#frame');
-		var elem1 = $('<div class="imgs back"></div>');
-		if (i > 7) {
-			i -= 8;
-		}
 
-		var img = $('<img class="img1" id="'+elem+'">');
-		var rnd = randomize(0, imagesCopy.length - 1);
-		img.attr("src", imagesCopy[rnd]);
-		imagesCopy.splice(rnd, 1);
-		if (imagesCopy.length === 0){
-			imagesCopy = images.slice(0);
-		}
-
-		elem1.append(img);
-		frame.append(elem1);
-	});
-	$(document).ready(function () {
-	$(".imgs").click(newGame);
-});
-	$('.img1').hide();
-	$("#hide").hide();
-}
-
-var newGame = Game()
-
-
-
-function Game() {
-
-var c = 2
-var click1;
-var arr = []
-var best;
-var attempt = 1
-var end = 0
- return function show(event){
-	var x = event.currentTarget.firstElementChild;
-	arr.push(x)
-	var arr0 = arr[0]
-	var arr1 = arr[1]
-	if (arr1 && arr0.id === arr1.id) {
-		c++;
-		arr.pop();
-	} else $(x).show(); $(x).parent().removeClass("back");
-	c -= 1
-	if (c === 0) {
-		if (arr[0].src !== arr[1].src) {
-			$("#hide").show();
-			setTimeout(function () { $(arr0).hide(); $(arr1).hide(); $("#hide").hide(); $(arr0).parent().addClass("back"); $(arr1).parent().addClass("back") }, 600);
-		} else {$(arr0).parent().attr("onclick", null).off("click"); $(arr0).parent().css("background-color", "green"); $(arr1).parent().attr("onclick", null).off("click"); $(arr1).parent().addClass("correct"); end++}
-		arr = []
-		c = 2
-		$("h2").html("Score: "+attempt++)
-		if (end === 0) {
-			end = 0
-			var restart = $("<input id='restart' type='button' value='Start again'>")
-			$('#frame').append(restart)
-			$('#restart').on('click',function() { console.log("hello"); create(range(1, 17)) } )
-		}
-	}	
-}
-}
-// create the game interface
-var images = ["https://www.flaticon.com/svg/static/icons/svg/3456/3456656.svg", "https://www.flaticon.com/svg/static/icons/svg/3456/3456709.svg","https://www.flaticon.com/svg/static/icons/svg/3456/3456669.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456847.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456890.svg","https://www.flaticon.com/premium-icon/icons/svg/3456/3456805.svg","https://www.flaticon.com/premium-icon/icons/svg/3457/3457010.svg", "https://www.flaticon.com/premium-icon/icons/svg/3456/3456953.svg" ]
 
 
 
--- script.js
+++ script.js
@@ -1,5 +1,102 @@
-create(range(1, 17))
+// executing function:
+// ==========================
+// starting the game the first time
+create(range(16))
+// creating game loop
+var newGame = Game(images)
 
-/*
-*/
 
+// creating and recreating the game interface
+// ======================================================
+function create(arr) {
+
+	// making copy of img sources
+	var imagesCopy = images.slice(0);
+	// removing all elements from frame
+	$('#frame').empty();
+	$('#restart').remove()
+	$("#score").html("Score: 0")
+	// itirating over id arr
+	each(arr, function (elem, i) {
+		
+		var frame = $('#frame');
+		// creating img containers
+		var elem1 = $('<div class="imgs back"></div>');
+
+		// creating imgs
+		var img = $('<img class="img1" id="'+elem+'">');
+		// random index for selecting an elem from the arr
+		var rnd = randomize(0, imagesCopy.length - 1);
+		img.attr("src", imagesCopy[rnd]);
+		// removing the selected element from the copy
+		imagesCopy.splice(rnd, 1);
+
+		// appending the imgs into frame
+		elem1.append(img);
+		frame.append(elem1);
+	});
+	// asigning img containers onclick to the new game loop
+	$(document).ready(function () {
+	$(".imgs").click(newGame);
+	});
+	// hiding imgs on game start
+	$('.img1').hide();
+	$("#hide").hide();
+}
+
+// defining game loop
+function Game(imgArr) {
+	//click counter
+	var counter = 2
+	var arr = []
+	var attempt = 0
+	var end = 0
+	var best = 999999
+	var fin = imgArr.length / 2
+	return function(event){
+		// current img tag variable
+		var x = event.currentTarget.firstElementChild;
+		arr.push(x)
+		var arr0 = arr[0]
+		var arr1 = arr[1]
+		// case for clicking same img twice
+		if (arr1 && arr0.id === arr1.id) {
+			counter++;
+			arr.pop();
+		} /*display clicked img*/ else $(x).show(); $(x).parent().removeClass("back");
+		counter -= 1
+		// when trying to match 2 imgs
+		if (counter === 0) {
+			// chack if the 2 imgs are the same
+			if (arr[0].src !== arr[1].src) {
+				// if they are not, give the player a some time to view them then hide them again
+				$("#hide").show();
+				setTimeout(function () { $(arr0).hide(); $(arr1).hide(); $("#hide").hide(); $(arr0).parent().addClass("back"); $(arr1).parent().addClass("back") }, 500);
+
+			} /*if they are the same do all this-->*/ else {$(arr0).parent().attr("onclick", null).off("click"); $(arr0).parent().css("background-color", "green"); $(arr1).parent().attr("onclick", null).off("click"); $(arr1).parent().addClass("correct"); end++}
+			// after game loop ends, reset these vaslues
+			arr = []
+			counter = 2
+			$("#score").html("Score: "+(++attempt))
+			// check if the player found all imgs
+			if (end === fin) {
+				// if so, create a restart button
+				var restart = $("<input id='restart' type='button' value='Start again'>")
+				$('#frame').after(restart)
+				$('#restart').on('click',function() { create(range(imgArr.length)) })
+				// and check personal score
+				if (attempt < best) {
+					best = attempt
+
+					// DOM method:
+					document.getElementById("best").innerHTML = 'Personal Best: '+attempt
+				}
+				// at the end of the game reset these values
+				attempt = 0
+				end = 0
+				arr = []
+				counter = 2
+			}
+		}
+	}
+}
